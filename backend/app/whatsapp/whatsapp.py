bf768deae584c2f9269ba8923d6e175dd1ce6483
Yashver1 <yashvershori@gmail.com>
2025-03-06 15:46:20 +0800

    add whatsapp webook

--- backend/app/whatsapp/whatsapp.py
+++ backend/app/whatsapp/whatsapp.py
@@ -17,13 +17,17 @@ class WhatsappMessage(BaseModel):
 async def whatsapp_webhook(message: WhatsappMessage):
     body = {
         "messaging_product": "whatsapp",
+        "recipient_type": "individual",
         "to": "6597714873",
         "type": "text",
         "text": {
+                "preview_url" : True,
                 "body": f"{message.message}"
         }
     }
-    r = requests.post(settings.whatsapp_web, headers=headers)
+    r = requests.post(settings.whatsapp_web, json=body, headers=headers)
+    print(r.status_code)
+    print(r.text)
     if r.status_code >= 300:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+        raise HTTPException(status_code=500, detail=f"{r.text}")
     return {200 : {"description" : "sucessfully sent message"}}
