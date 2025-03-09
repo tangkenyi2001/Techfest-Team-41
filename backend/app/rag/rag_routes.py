3f7f45001c2fc1b22b0c45eb71f8213796bf28b4
I757499 <ken.yi.tang@sap.com>
2025-03-09 15:39:51 +0800

    updated scraping for js

--- backend/app/rag/rag_routes.py
+++ backend/app/rag/rag_routes.py
@@ -6,8 +6,8 @@ from langgraph.prebuilt import create_react_agent
 from langchain_core.tools import tool
 from fastapi import APIRouter
 from fastapi import APIRouter, Depends, HTTPException
-<<<<<<< HEAD
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+from fastapi.responses import JSONResponse
 from typing import List
 from langchain_core.tools import StructuredTool
 import bs4
@@ -32,30 +32,13 @@ from crawl4ai.content_filter_strategy import PruningContentFilter
 from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
 from langchain_core.tools import StructuredTool
 from langchain.tools import Tool
-=======
-from pydantic import BaseModel,Field
-from fastapi.responses import JSONResponse
->>>>>>> c09a4d28f6613c49ebc780a31cdf2e166309261c
 load_dotenv()
 
-router = APIRouter(prefix="/rag",tags=["rag"])
+router = APIRouter(tags=["RAGCompletion"])
 
 class userMessage(BaseModel):
     message: str
 
-<<<<<<< HEAD
-@router.post("/rag")
-async def rag(request: userMessage)->str:
-    try:
-        model = init_chat_model("gpt-4", model_provider="openai")
-        tools=[query]
-        model_with_tools = model.bind_tools(tools)
-        
-        agent_executor = create_react_agent(model, tools)   
-        response = await agent_executor.ainvoke({"messages": [HumanMessage(content=request.message)]})
-        return response["messages"][-1].pretty_repr()
-
-=======
 class ResponseFormatter(BaseModel):
     """Always use this tool to structure your response to the user."""
     verdict: str = Field(description="The answer to the user's question,{Topic} is REAL/FAKE")
@@ -76,6 +59,5 @@ async def rag(request: userMessage)->str:
         
         # Return a proper JSONResponse
         return JSONResponse(content=response_dict)
->>>>>>> c09a4d28f6613c49ebc780a31cdf2e166309261c
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
